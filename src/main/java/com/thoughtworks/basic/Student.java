2ae8d3fe865e13b4d89c849730c150760ada5337
luna <1661639144@qq.com>
2020-07-15 11:16:28 +0800

    luna finish 第二题

--- src/main/java/com/thoughtworks/basic/Student.java
+++ src/main/java/com/thoughtworks/basic/Student.java
@@ -1,15 +1,12 @@
 package com.thoughtworks.basic;
 
-public class Student {
-    private String name;
-    private int age;
+public class Student extends Person{
     private  ClassNumber classNumber;
     public Student(String name,int age,ClassNumber classNumber){
-        this.name=name;
-        this.age=age;
+        super(name,age);
         this.classNumber=classNumber;
     }
     public void  introduce(){
-        System.out.println("My name is "+name+",I am "+age+"years old. I am a Student of Class "+classNumber.getKlass());
+        System.out.println("My name is "+getName()+",I am "+getAge()+"years old. I am a Student of Class "+classNumber.getKlass());
     }
 }
